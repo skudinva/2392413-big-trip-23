04e63512b40e25fa0308a19782edabd877e09f63
Skudin Vladimir <skudinva@gmail.com>
2024-05-07 15:57:54 +0700

    Оптимизировал функцию

--- src/utils.js
+++ src/utils.js
@@ -21,6 +21,7 @@ const getMachinizeDateTime = (date) =>
 const getShortTime = (date) => getDateString(date, DateFormat.SHORT_TIME);
 const getInputDateTime = (date) =>
   getDateString(date, DateFormat.INPUT_DATETIME);
+
 const getDurationString = (dateFrom, dateTo) => {
   if (!dateFrom || !dateTo) {
     return '';
@@ -31,29 +32,27 @@ const getDurationString = (dateFrom, dateTo) => {
     minutes: 0,
     hours: 0,
     days: 0,
-    strings: [],
+    getHumanizeString: function () {
+      const strings = [];
+      strings.push(`${String(this.minutes).padStart(2, '0')}M`);
+      if (this.hours > 0 || this.days > 0) {
+        strings.unshift(`${String(this.hours).padStart(2, '0')}H`);
+      }
+
+      if (this.days > 0) {
+        strings.unshift(`${String(this.days).padStart(2, '0')}D`);
+      }
+      return strings.join(' ');
+    },
   };
 
   duration.minutes = date1.diff(date2, 'm');
-  if (duration.minutes >= 60) {
-    duration.hours = Math.floor(duration.minutes / 60);
-    duration.minutes -= duration.hours * 60;
-  }
-  if (duration.hours >= 24) {
-    duration.days = Math.floor(duration.hours / 24);
-    duration.hours -= duration.days * 24;
-  }
-  duration.strings.push(`${String(duration.days).padStart(2, '0')}D`);
-  duration.strings.push(`${String(duration.hours).padStart(2, '0')}H`);
-  duration.strings.push(`${String(duration.minutes).padStart(2, '0')}M`);
-  duration.strings.every((element, index) => {
-    if (element.startsWith('00')) {
-      delete duration.strings[index];
-      return true;
-    }
-    return false;
-  });
-  return duration.strings.join(' ');
+  duration.hours = Math.floor(duration.minutes / 60);
+  duration.minutes = duration.minutes % 60;
+  duration.days = Math.floor(duration.hours / 24);
+  duration.hours = duration.hours % 24;
+
+  return duration.getHumanizeString();
 };
 
 const getPeriodString = (dateFrom, dateTo, delimiter) => {
