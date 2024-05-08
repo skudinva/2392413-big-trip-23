6fd37429165d70184aaa6358464acff3a728fde0
Skudin Vladimir <skudinva@gmail.com>
2024-05-08 18:02:51 +0700

    Обработал фильтры и передал результат во View

--- src/filter.js
+++ src/filter.js
@@ -1,10 +1,18 @@
+import dayjs from 'dayjs';
 import { FilterType } from './const';
 
 const filter = {
   [FilterType.EVERYTHING]: (events) => events.filter((event) => event),
-  [FilterType.FUTURE]: (events) => events.filter((event) => event),
-  [FilterType.PAST]: (events) => events.filter((event) => event),
-  [FilterType.PRESENT]: (events) => events.filter((event) => event),
+  [FilterType.FUTURE]: (events) =>
+    events.filter((event) => dayjs().isBefore(dayjs(event.dateFrom))),
+  [FilterType.PAST]: (events) =>
+    events.filter((event) => dayjs().isAfter(dayjs(event.dateFrom))),
+  [FilterType.PRESENT]: (events) =>
+    events.filter(
+      (event) =>
+        dayjs().isAfter(dayjs(event.dateFrom)) &&
+        dayjs().isBefore(dayjs(event.dateTo))
+    ),
 };
 
 function generateFilter(events) {
