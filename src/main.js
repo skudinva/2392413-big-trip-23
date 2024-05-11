553e4a74f4ae511e74d3a0790bd15f409b673ba7
Skudin Vladimir <skudinva@gmail.com>
2024-05-11 22:45:39 +0700

    Рефакторинг для связывания презентеров фильтрации и основного

--- src/main.js
+++ src/main.js
@@ -3,43 +3,37 @@ import EventPresenter from './presenter/event-presenter';
 import FilterPresenter from './presenter/filter-presenter';
 import TripPresenter from './presenter/trip-presenter';
 
+const headerContainer = document.querySelector(
+  '.page-header .page-header__container'
+);
+const tripContainerElement = headerContainer.querySelector('.trip-main');
+const tripControlElement = headerContainer.querySelector(
+  '.trip-controls__filters'
+);
+
+const eventsContainerElement = document.querySelector(
+  '.page-main .trip-events'
+);
+const newEventButtonElement = document.querySelector(
+  '.trip-main__event-add-btn'
+);
+
 const eventsModel = new EventsModel();
 
-const renderHeader = () => {
-  const headerContainer = document.querySelector(
-    '.page-header .page-header__container'
-  );
-  const tripControlElement = headerContainer.querySelector(
-    '.trip-controls__filters'
-  );
+const tripPresenter = new TripPresenter({
+  container: tripContainerElement,
+  eventsModel,
+});
+const filterPresenter = new FilterPresenter({
+  container: tripControlElement,
+  eventsModel,
+});
+const eventPresenter = new EventPresenter({
+  container: eventsContainerElement,
+  eventsModel,
+  newEventButtonElement: newEventButtonElement,
+});
 
-  const containerElement = headerContainer.querySelector('.trip-main');
-  const tripPresenter = new TripPresenter({
-    container: containerElement,
-    eventsModel,
-  });
-  tripPresenter.init();
-
-  const filterPresenter = new FilterPresenter({
-    container: tripControlElement,
-    eventsModel,
-  });
-  filterPresenter.init();
-};
-
-const renderMain = () => {
-  const containerElement = document.querySelector('.page-main .trip-events');
-  const newEventButtonElement = document.querySelector(
-    '.trip-main__event-add-btn'
-  );
-
-  const eventPresenter = new EventPresenter({
-    container: containerElement,
-    eventsModel,
-    newEventButtonElement: newEventButtonElement,
-  });
-  eventPresenter.init();
-};
-
-renderHeader();
-renderMain();
+tripPresenter.init();
+filterPresenter.init();
+eventPresenter.init();
