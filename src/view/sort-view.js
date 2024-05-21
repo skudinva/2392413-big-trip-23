59ff9240156be5586788ef35c16558c3e74a3221
Skudin Vladimir <skudinva@gmail.com>
2024-05-21 23:59:00 +0700

    форматирование

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -5,10 +5,11 @@ const createSortItemTemplate = (currentSortType, { type, label, disabled }) => {
   const additionProps =
     (type === currentSortType ? 'checked' : ' ') +
     (disabled ? 'disabled' : ' ');
+
   return `<div class="trip-sort__item  trip-sort__item--${type}">
-<input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${additionProps}>
-<label class="trip-sort__btn" for="sort-${type}">${label}</label>
-</div>`;
+    <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${additionProps}>
+    <label class="trip-sort__btn" for="sort-${type}">${label}</label>
+    </div>`;
 };
 
 const createSortTemplate = (currentSortType) => {
