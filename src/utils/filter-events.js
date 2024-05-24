37ab6d167ac85bacb647bb4333c89c587e8c4ece
Skudin Vladimir <skudinva@gmail.com>
2024-05-24 15:55:42 +0700

    сортировка фильтров

--- src/utils/filter-events.js
+++ src/utils/filter-events.js
@@ -5,14 +5,15 @@ const filterEvents = {
   [FilterType.EVERYTHING]: (events) => events.filter((event) => event),
   [FilterType.FUTURE]: (events) =>
     events.filter((event) => dayjs().isBefore(dayjs(event.dateFrom))),
-  [FilterType.PAST]: (events) =>
-    events.filter((event) => dayjs().isAfter(dayjs(event.dateFrom))),
+
   [FilterType.PRESENT]: (events) =>
     events.filter(
       (event) =>
         dayjs().isAfter(dayjs(event.dateFrom)) &&
         dayjs().isBefore(dayjs(event.dateTo))
     ),
+  [FilterType.PAST]: (events) =>
+    events.filter((event) => dayjs().isAfter(dayjs(event.dateFrom))),
 };
 
 export { filterEvents };
